6eb5c54e3291424b566c8583a61aafa04386278f
Pavel Litvinenko <p.litvinenko@start.ru>
2021-06-29 00:19:44 +0300

    add umbrella and headermap

---
+++ iOSTagLib-umbrella.h
@@ -0,0 +1,18 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+#import "NSURL+TagLib.h"
+
+FOUNDATION_EXPORT double iOSTagLibVersionNumber;
+FOUNDATION_EXPORT const unsigned char iOSTagLibVersionString[];
+
